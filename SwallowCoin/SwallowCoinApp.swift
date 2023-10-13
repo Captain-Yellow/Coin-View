bb540aa7fcc9d6934de898728d066a41814dbf67
Mohammad Afshar <neo.mohammad.afshar@gmail.com>
2023-10-13 13:42:23 +0330

    Initial Commit

---
+++ SwallowCoin/SwallowCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwallowCoinApp.swift
+//  SwallowCoin
+//
+//  Created by Mohammad Afshar on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwallowCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
